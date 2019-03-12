95239e8b3ee0ea71bc2db3cdccd5a5c3127b135a
Brett <langfordbrett@gmail.com>
2019-03-12 18:54:02 -0500

    update flash.js
    Changed the style for flash.js and am testing branches in GitHub

--- flash.js
+++ flash.js
@@ -1,31 +1,33 @@
-let color1 = document.getElementById("box1");
-let color2 = document.getElementById("box2");
-let img = document.getElementById("hedgehog")
+let color1 = document.getElementById('box1')
+let color2 = document.getElementById('box2')
+let img = document.getElementById('hedgehog')
 let theta = 0
 let i = 0
+let phi = 0
 
-function flashColors() {
-    spinHedgehog();
+function flashColors () {
+  spinHedgehog()
 
-    if (i % 2 == 0) {
-        color1.style.backgroundColor = "green";
-        color2.style.backgroundColor = "purple";
-        i++
-    } else {
-        color1.style.backgroundColor = "purple";
-        color2.style.backgroundColor = "green";
-        i++
-    }
+  if (i % 2 === 0) {
+    color1.style.backgroundColor = 'green'
+    color2.style.backgroundColor = 'purple'
+    i++
+  } else {
+    color1.style.backgroundColor = 'purple'
+    color2.style.backgroundColor = 'green'
+    i++
+  }
 }
 
 function calculateAngle (theta) {
-    phi = 'rotate(' + theta.toString() + 'deg)';
-    return phi
-}
-function spinHedgehog() {
-    theta += 10;
-    phi = calculateAngle(theta);
-    img.style.transform = phi;
+  phi = 'rotate(' + theta.toString() + 'deg)'
+  return phi
 }
 
-let killMe = setInterval(flashColors, 30);
+function spinHedgehog () {
+  theta += 10
+  phi = calculateAngle(theta)
+  img.style.transform = phi
+}
+
+setInterval(flashColors, 30)
